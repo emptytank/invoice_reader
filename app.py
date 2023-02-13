ae746743d0496d05908502d9e454e8475c8076f7
emptytank <41249957+emptytank@users.noreply.github.com>
2023-02-13 01:37:40 -0800

    updated app

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import os
 import streamlit as st
+from PIL import Image
 from invoice_reader.functions import extract_pdf_contents, provide_answer
 
 try:
 
-    # # Set API key
-    # os.getenv("OPENAI_API_KEY") == st.secrets["OPENAI_API_KEY"]
+    os.getenv("OPENAI_API_KEY") == st.secrets["OPENAI_API_KEY"]
 
     # Set path for pdf file
-    path = "./src/Invoice1.pdf"
+    pdf_path = "./src/Invoice1.pdf"
 
     # Read pdf file
-    pdf_contents = extract_pdf_contents(path)
+    pdf_contents = extract_pdf_contents(pdf_path)
 
+    # Set path for image file
+    image= Image.open('./src/Invoice1.png')
+
+    # Display the pdf image
+    st.image(image=image)
+        
     # Initialize state variable
     if "read" not in st.session_state:
         st.session_state["read"] = ""
-
-    st.title("Invoice Reader")
 
     # Create an input text box for the user to enter the question
     input_text = st.text_area(label="Enter a question about the invoice:", value="", height=250)
@@ -31,7 +35,6 @@ try:
 
     # Create an output text box that shows the current state of the output
     answer = st.text_area(label="Answer:", value=st.session_state["read"], height=250)
-
 
 except:
     st.write("There was an error.")
